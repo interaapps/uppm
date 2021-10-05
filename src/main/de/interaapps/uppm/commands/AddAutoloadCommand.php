2cbab5b043484eb4aa8eb31114612541cd28ba92
Julian Gojani <julian@gojani.xyz>
2021-10-05 17:27:04 +0200

    Added repl, a uppm.phar-creation workflow and more

--- src/main/de/interaapps/uppm/commands/AddAutoloadCommand.php
+++ src/main/de/interaapps/uppm/commands/AddAutoloadCommand.php
@@ -15,7 +15,7 @@ class AddAutoloadCommand implements Command {
 
     public function execute(array $args) {
         $this->uppm->getLogger()->info("Downloading autoload.php...");
-        file_put_contents($this->uppm->getCurrentProject()->getDir()."autoload.php", file_get_contents("https://raw.githubusercontent.com/interaapps/uppm/master/autoload.php"));
+        file_put_contents($this->uppm->getCurrentProject()->getDir()."/autoload.php", file_get_contents("https://raw.githubusercontent.com/interaapps/uppm/master/autoload.php"));
     }
 
 }
